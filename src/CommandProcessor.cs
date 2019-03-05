ce2ee302dc54c8081675d8c9e08c407807ab9745
beletsky.anton <beletsky.anton@apriorit.com>
2019-03-05 19:29:38 +0200

    added ICommandProcessor and IQueryProcessor implementations

---
+++ src/CommandProcessor.cs
@@ -0,0 +1,46 @@
+﻿namespace Cqrs.Core
+{
+    #region Using
+    using Cqrs.Core.Abstractions;
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    #endregion
+
+    public class CommandProcessor : ICommandProcessor
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public CommandProcessor(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Execute(ICommand command)
+        {
+            var handlerType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Command handler is not registred for command of type { command.GetType().Name }");
+            handlerType.GetTypeInfo().GetDeclaredMethod("Execute").Invoke(handler, new object[] { command });
+        }
+
+        public TResult Execute<TResult>(ICommand<TResult> command)
+        {
+            var handlerType = typeof(ICommandHandler<,>).MakeGenericType(command.GetType(), typeof(TResult));
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Command handler is not registred for command of type { command.GetType().Name }");
+            return (TResult)handlerType.GetTypeInfo().GetDeclaredMethod("Execute").Invoke(handler, new object[] { command });
+        }
+
+        public async Task ExecuteAsync(ICommand command)
+        {
+            var handlerType = typeof(ICommandHandlerAsync<>).MakeGenericType(command.GetType());
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Async Command handler is not registred for command of type { command.GetType().Name }");
+            await(Task) handlerType.GetTypeInfo().GetDeclaredMethod("ExecuteAsync").Invoke(handler, new object[] { command });
+        }
+
+        public async Task<TResult> ExecuteAsync<TResult>(ICommand<TResult> command)
+        {
+            var handlerType = typeof(ICommandHandlerAsync<,>).MakeGenericType(command.GetType(), typeof(TResult));
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Async Command handler is not registred for command of type { command.GetType().Name }");
+            return await(Task<TResult>) handlerType.GetTypeInfo().GetDeclaredMethod("ExecuteAsync").Invoke(handler, new object[] { command });
+        }
+    }
+}
---
+++ src/QueryProcessor.cs
@@ -0,0 +1,32 @@
+﻿namespace Cqrs.Core
+{
+    #region Using
+    using Cqrs.Core.Abstractions;
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    #endregion
+
+    public class QueryProcessor : IQueryProcessor
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public QueryProcessor(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public TResult Execute<TResult>(IQuery<TResult> query)
+        {
+            var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResult));
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Query handler is not registred for query of type { query.GetType().Name }");
+            return (TResult)handlerType.GetTypeInfo().GetDeclaredMethod("Query").Invoke(handler, new object[] { query });
+        }
+
+        public async Task<TResult> ExecuteAsync<TResult>(IQuery<TResult> query)
+        {
+            var handlerType = typeof(IQueryHandlerAsync<,>).MakeGenericType(query.GetType(), typeof(TResult));
+            var handler = _serviceProvider.GetService(handlerType) ?? throw new ArgumentNullException($"Async Query handler is not registred for query of type { query.GetType().Name }");
+            return await (Task<TResult>)handlerType.GetTypeInfo().GetDeclaredMethod("QueryAsync").Invoke(handler, new object[] { query });
+        }
+    }
+}
