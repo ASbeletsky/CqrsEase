18b73d5f47819f25d19bbe2738308056d094e504
beletsky.anton <beletsky.anton@apriorit.com>
2019-03-11 17:26:19 +0200

    added identifiable abstaction and moved source code to Cqrs.Core folder

---
+++ src/Cqrs.Model.Abstractions/Identifiable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cqrs.Model.Abstractions
+{
+    public interface Identifiable
+    {
+        object Id { get; }
+    }
+
+    public interface Identifiable<out TKey> : Identifiable
+    {
+        new TKey Id { get; }
+    }
+}
---
+++ src/Cqrs.Model.Abstractions/IdentifiableModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cqrs.Model.Abstractions
+{
+    public abstract class IdentifiableModel<TKey> : Identifiable<TKey>
+    {
+        public TKey Id { get; set; }
+
+        object Identifiable.Id => this.Id;
+    }
+}
