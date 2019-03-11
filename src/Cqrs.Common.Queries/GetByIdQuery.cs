f5c2d06f2fd152e9c83ebc1978db673d57a60fdc
beletsky.anton <beletsky.anton@apriorit.com>
2019-03-11 19:59:06 +0200

    added GetByIdQuery

---
+++ src/Cqrs.Common.Queries/GetByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace Cqrs.Common.Queries
+{
+    using Cqrs.Core.Abstractions;
+    using Cqrs.Model.Abstractions;
+
+    public class GetByIdQuery<TKey, TEntity> : IQuery<TEntity>
+            where TEntity : class, Identifiable<TKey>
+    {
+        public TKey Id { get; private set; }
+
+        public GetByIdQuery(TKey id)
+        {
+            Id = id;
+        }
+    }
+}
