716e0c52967acf7655d1ec81b0f3bed09cc1c5de
beletsky.anton <beletsky.anton@apriorit.com>
2019-03-22 16:02:34 +0200

    added find query

---
+++ src/Cqrs.Common.Queries/FindFirstQuery.cs
@@ -0,0 +1,30 @@
+﻿using Cqrs.Common.Queries.Sorting;
+using Cqrs.Core.Abstractions;
+using Cqrs.Model.Abstractions;
+using NSpecifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cqrs.Common.Queries
+{
+    public class FindFirstQuery<T, TSpecification> : IQuery<T>
+        where TSpecification : ISpecification<T>
+    {
+        public FindFirstQuery(TSpecification specification)
+            : this(specification, null)
+        {
+
+        }
+
+        public FindFirstQuery(TSpecification specification, IOrderedFetchStrategy<T> fetchStrategy)
+        {
+            Specification = specification;
+            FetchStrategy = fetchStrategy;
+        }
+
+        public TSpecification Specification { get; }
+
+        public IOrderedFetchStrategy<T> FetchStrategy { get; set; }
+    }
+}
