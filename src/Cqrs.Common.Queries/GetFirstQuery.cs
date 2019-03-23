642ef97c4364776c7569d8fdac95bee0bd0bdac8
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 18:18:40 +0200

    add ProjectFirstQuery

---
+++ src/Cqrs.Common.Queries/GetFirstQuery.cs
@@ -0,0 +1,43 @@
+﻿namespace Cqrs.Common.Queries
+{
+    #region Using
+    using Cqrs.Common.Queries.ProjectionStateries;
+    using Cqrs.Common.Queries.Sorting;
+    using Cqrs.Core.Abstractions;
+    using NSpecifications;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    #endregion
+
+    public class GetFirstQuery<T> : IQuery<T>
+    {
+        public GetFirstQuery(ISpecification<T> specification)
+            : this(specification, fetchStrategy: new FetchAllStatery<T>())
+        {
+
+        }
+
+        public GetFirstQuery(ISpecification<T> specification, IFetchStrategy<T> fetchStrategy)
+            : this(specification, fetchStrategy, null)
+        {
+
+        }
+
+        public GetFirstQuery(ISpecification<T> specification, IFetchStrategy<T> fetchStrategy, Expression<Func<T, object>> sortKeySelector)
+        {
+            Specification = specification;
+            FetchStrategy = fetchStrategy;
+            if (sortKeySelector != null)
+            {
+                Sorting = new OrderCreteria<T>[] { new OrderCreteria<T>(sortKeySelector, OrderDirection.ASC) };
+            }
+        }
+
+        public ISpecification<T> Specification { get; }
+
+        public IFetchStrategy<T> FetchStrategy { get; }
+
+        public IEnumerable<OrderCreteria<T>> Sorting { get; }
+    }
+}
---
+++ src/Cqrs.Common.Queries/ProjectFirstQuery.cs
@@ -0,0 +1,18 @@
+﻿namespace Cqrs.Common.Queries
+{
+    #region Using
+    using NSpecifications;
+    using System;
+    using System.Linq.Expressions;
+
+    public class ProjectFirstQuery<TSource, TDest> : GetFirstQuery<TDest>
+    {
+        public ProjectFirstQuery(ISpecification<TDest> specification) : base(specification)
+        {
+        }
+
+        public ProjectFirstQuery(ISpecification<TDest> specification, Expression<Func<TDest, object>> sortKeySelector) : base(specification, null, sortKeySelector)
+        {
+        }
+    }
+}
