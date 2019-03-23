88c0102f19180207d53f13c232c84367e3b034c2
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 16:53:59 +0200

    added ExistsQuery

---
+++ src/Cqrs.Common.Queries/ExistsQuery.cs
@@ -0,0 +1,16 @@
+﻿using Cqrs.Core.Abstractions;
+using NSpecifications;
+
+namespace Cqrs.Common.Queries
+{
+    public class ExistsQuery<T, TSpecification> : IQuery<bool>
+            where TSpecification : ISpecification<T>
+    {
+        public ExistsQuery(TSpecification specification)
+        {
+            Specification = specification;
+        }
+
+        public TSpecification Specification { get; }
+    }
+}
