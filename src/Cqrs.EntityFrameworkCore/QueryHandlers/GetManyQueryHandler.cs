ecfad8a314579d85d81f62756a9d08b8a059d468
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 22:53:51 +0200

    added habdlers for GetMany and ProjectMany queries

---
+++ src/Cqrs.EntityFrameworkCore/QueryHandlers/GetManyQueryHandler.cs
@@ -0,0 +1,61 @@
+﻿namespace Cqrs.EntityFrameworkCore.QueryHandlers
+{
+    #region Using
+    using Cqrs.Common.Queries;
+    using Cqrs.Common.Queries.Pagination;
+    using Cqrs.Core.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    #endregion
+
+    internal class GetManyQueryHandler<T>
+        : IQueryHandler<GetManyQuery<T>, IEnumerable<T>>
+        , IQueryHandler<GetManyQuery<T>, ILimitedEnumerable<T>>
+        , IQueryHandlerAsync<GetManyQuery<T>, IEnumerable<T>>
+        , IQueryHandlerAsync<GetManyQuery<T>, ILimitedEnumerable<T>>
+        where T : class
+    {
+        public GetManyQueryHandler(EfDataSourceBased dataSource)
+        {
+            DataSource = dataSource;
+        }
+
+        public EfDataSourceBased DataSource { get; }
+
+        protected virtual IQueryable<T> PrepareFilter(GetManyQuery<T> query)
+        {
+            return DataSource.Query<T>().MaybeWhere(query.Specification);
+        }
+
+        protected IQueryable<T> PrepareDataQuery(GetManyQuery<T> query)
+        {
+            return PrepareFilter(query).MaybeSort(query.Sorting).MaybeTake(query.Pagination).ApplyFetchStrategy(query.FetchStrategy);
+        }
+
+        public IEnumerable<T> Request(GetManyQuery<T> query)
+        {
+            return PrepareDataQuery(query).ToList();
+        }
+
+        public async Task<IEnumerable<T>> RequestAsync(GetManyQuery<T> query)
+        {
+            return await PrepareDataQuery(query).ToListAsync();
+        }
+
+        ILimitedEnumerable<T> IQueryHandler<GetManyQuery<T>, ILimitedEnumerable<T>>.Request(GetManyQuery<T> query)
+        {
+            var count = PrepareFilter(query).Count();
+            IEnumerable<T> data = count > 0 ? Request(query) : Enumerable.Empty<T>();
+            return new LimitedEnumerable<T>(data, count);
+        }
+
+        async Task<ILimitedEnumerable<T>> IQueryHandlerAsync<GetManyQuery<T>, ILimitedEnumerable<T>>.RequestAsync(GetManyQuery<T> query)
+        {
+            var count = await PrepareFilter(query).CountAsync();
+            IEnumerable<T> data = count > 0 ? await RequestAsync(query) : Enumerable.Empty<T>();
+            return new LimitedEnumerable<T>(data, count);
+        }
+    }
+}
---
+++ src/Cqrs.EntityFrameworkCore/QueryHandlers/ProjectManyQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using Cqrs.Common.Queries;
+using Cqrs.Common.Queries.Pagination;
+using Cqrs.Core.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cqrs.EntityFrameworkCore.QueryHandlers
+{
+    internal class ProjectManyQueryHandler<TSource, TDest>
+        : GetManyQueryHandler<TDest>
+        , IQueryHandler<ProjectManyQuery<TSource, TDest>, IEnumerable<TDest>>
+        , IQueryHandler<ProjectManyQuery<TSource, TDest>, ILimitedEnumerable<TDest>>
+        , IQueryHandlerAsync<ProjectManyQuery<TSource, TDest>, IEnumerable<TDest>>
+        , IQueryHandlerAsync<ProjectManyQuery<TSource, TDest>, ILimitedEnumerable<TDest>>
+        where TSource : class
+        where TDest : class
+    {
+        public IProjector Projector { get; }
+
+        public ProjectManyQueryHandler(EfDataSourceBased dataSource, IProjector projector) : base(dataSource)
+        {
+            Projector = projector;
+        }
+
+        protected override IQueryable<TDest> PrepareFilter(GetManyQuery<TDest> query)
+        {
+            return Projector.ProjectTo<TDest>(DataSource.Query<TSource>()).MaybeWhere(query.Specification);
+        }
+
+        public IEnumerable<TDest> Request(ProjectManyQuery<TSource, TDest> query)
+        {
+            return base.Request(query);
+        }
+
+        public async Task<IEnumerable<TDest>> RequestAsync(ProjectManyQuery<TSource, TDest> query)
+        {
+            return await base.RequestAsync(query);
+        }
+
+        ILimitedEnumerable<TDest> IQueryHandler<ProjectManyQuery<TSource, TDest>, ILimitedEnumerable<TDest>>.Request(ProjectManyQuery<TSource, TDest> query)
+        {
+            return (this as IQueryHandler<GetManyQuery<TDest>, ILimitedEnumerable<TDest>>).Request(query);
+        }
+
+        async Task<ILimitedEnumerable<TDest>> IQueryHandlerAsync<ProjectManyQuery<TSource, TDest>, ILimitedEnumerable<TDest>>.RequestAsync(ProjectManyQuery<TSource, TDest> query)
+        {
+            return await (this as IQueryHandlerAsync<GetManyQuery<TDest>, ILimitedEnumerable<TDest>>).RequestAsync(query);
+        }
+    }
+}
