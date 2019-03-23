a23cce350b044d82d994dd7ff6816dfbf59d6b77
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 22:03:49 +0200

    added handlers for GetFirst and ProjectFirst queries

---
+++ src/Cqrs.EntityFrameworkCore/QueryHandlers/GetFirstQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace Cqrs.EntityFrameworkCore.QueryHandlers
+{
+    #region Using
+    using Cqrs.Common.Queries;
+    using Cqrs.Core.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+    using System.Threading.Tasks;
+    #endregion
+
+    internal class GetFirstQueryHandler<TEntity> 
+        : IQueryHandler<GetFirstQuery<TEntity>, TEntity>
+        , IQueryHandlerAsync<GetFirstQuery<TEntity>, TEntity>
+        where TEntity : class
+    {
+        public GetFirstQueryHandler(EfDataSourceBased dataSource)
+        {
+            DataSource = dataSource;
+        }
+
+        public EfDataSourceBased DataSource { get; }
+
+        protected IQueryable<TEntity> PrepareQuery(GetFirstQuery<TEntity> query)
+        {
+            return DataSource.Query<TEntity>()
+                .MaybeWhere(query.Specification)
+                .MaybeSort(query.Sorting)
+                .ApplyFetchStrategy(query.FetchStrategy);
+        }
+
+        public TEntity Request(GetFirstQuery<TEntity> query)
+        {
+            return PrepareQuery(query).FirstOrDefault();
+        }
+
+        public Task<TEntity> RequestAsync(GetFirstQuery<TEntity> query)
+        {
+            return PrepareQuery(query).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ src/Cqrs.EntityFrameworkCore/QueryHandlers/ProjectFirstQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace Cqrs.EntityFrameworkCore.QueryHandlers
+{
+    #region Using
+    using Cqrs.Common.Queries;
+    using Cqrs.Core.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+    using System.Threading.Tasks;
+    #endregion
+
+    internal class ProjectFirstQueryHandler<TSource, TDest>
+        : IQueryHandler<ProjectFirstQuery<TSource, TDest>, TDest>
+        , IQueryHandlerAsync<ProjectFirstQuery<TSource, TDest>, TDest>
+        where TSource : class
+    {
+        public ProjectFirstQueryHandler(EfDataSourceBased dataSource, IProjector projector)
+        {
+            DataSource = dataSource;
+            Projector = projector;
+        }
+
+        public EfDataSourceBased DataSource { get; }
+        public IProjector Projector { get; }
+
+        protected IQueryable<TDest> PrepareQuery(ProjectFirstQuery<TSource, TDest> query)
+        {
+            return Projector.ProjectTo<TDest>(DataSource.Query<TSource>()).MaybeWhere(query.Specification).MaybeSort(query.Sorting).ApplyFetchStrategy(query.FetchStrategy);
+        }
+
+        public TDest Request(ProjectFirstQuery<TSource, TDest> query)
+        {
+            return PrepareQuery(query).FirstOrDefault();
+        }
+
+        public Task<TDest> RequestAsync(ProjectFirstQuery<TSource, TDest> query)
+        {
+            return PrepareQuery(query).FirstOrDefaultAsync();
+        }
+    }
+}
