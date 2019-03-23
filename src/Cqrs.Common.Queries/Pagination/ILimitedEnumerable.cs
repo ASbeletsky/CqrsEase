d539602cfa820b2b43396086a0660153c845ca6a
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 22:52:47 +0200

    added ILimitedEnumerable implementation

--- src/Cqrs.Common.Queries/Pagination/ILimitedEnumerable.cs
+++ src/Cqrs.Common.Queries/Pagination/ILimitedEnumerable.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace Cqrs.Common.Queries
+namespace Cqrs.Common.Queries.Pagination
 {
     public interface ILimitedEnumerable<out T> : IEnumerable<T>
     {
---
+++ src/Cqrs.Common.Queries/Pagination/LimitedEnumerable.cs
@@ -0,0 +1,31 @@
+﻿namespace Cqrs.Common.Queries.Pagination
+{
+    #region
+    using System.Collections;
+    using System.Collections.Generic;
+    #endregion
+
+    public class LimitedEnumerable<T> : ILimitedEnumerable<T>
+    {
+        private readonly IEnumerable<T> _inner;
+        private readonly int _totalCount;
+
+        public LimitedEnumerable(IEnumerable<T> inner, int totalCount)
+        {
+            _inner = inner;
+            _totalCount = totalCount;
+        }
+
+        public int TotalCount => _totalCount;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _inner.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
