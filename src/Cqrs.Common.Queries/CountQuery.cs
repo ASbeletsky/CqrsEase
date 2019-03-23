983a3549e5be9889be6e473486460a93f247cde9
ASbeletsky <antonbeletsky@gmail,com>
2019-03-23 16:52:44 +0200

    add CountQuery

---
+++ src/Cqrs.Common.Queries/CountQuery.cs
@@ -0,0 +1,16 @@
+﻿using Cqrs.Core.Abstractions;
+using NSpecifications;
+
+namespace Cqrs.Common.Queries
+{
+    public class CountQuery<T, TSpecification> : IQuery<long>
+        where TSpecification : ISpecification<T>
+    {
+        public CountQuery(TSpecification specification)
+        {
+            Specification = specification;
+        }
+
+        public TSpecification Specification { get; }
+    }
+}
