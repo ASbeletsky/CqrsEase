e467f67afa170d135e05e3f94f1613bceaa1f8ce
ASbeletsky <antonbeletsky@gmail,com>
2019-03-25 01:35:50 +0200

    added queries registration

--- src/Cqrs.EntityFrameworkCore/ServiceContainer.cs
+++ src/Cqrs.EntityFrameworkCore/ServiceContainer.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Autofac;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Cqrs.Core;
+using Cqrs.Core.Abstractions;
 using Cqrs.EntityFrameworkCore.QueryHandlers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Cqrs.EntityFrameworkCore
 {
-    public class ServiceContainer : Autofac.Module
+    public static class MicrosoftDIExtensions 
     {
-        protected override void Load(ContainerBuilder builder)
+        public static IServiceCollection UseCqrsEntityFramework<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
         {
-            base.Load(builder);
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
 
-            builder.RegisterGeneric(typeof(GetFirstQueryHandler<>)).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<EfDataSourceBased>()
+                .WithParameter((param, ctx) => param.Name == "DbContext", (param, ctx) => ctx.Resolve<TDbContext>())
+                .InstancePerLifetimeScope();
+
+            builder.RegisterGeneric(typeof(GetFirstQueryHandler<>))
+                .WithParameter((param, ctx) => param.Name == "dataSource", (param, ctx) => ctx.Resolve<EfDataSourceBased>())
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            var serviceProvider = new AutofacServiceProvider(builder.Build());
+            services.AddScoped<IQueryProcessor, QueryProcessor>(x => new QueryProcessor(serviceProvider));
+            return services;
         }
     }
 }
