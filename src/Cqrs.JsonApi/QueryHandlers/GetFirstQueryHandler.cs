e0b613c1d89e7e4ea12836a54b4512b962485fcf
ASbeletsky <antonbeletsky@gmail,com>
2019-03-24 16:52:57 +0200

    added GetFirstQueryHandler

---
+++ src/Cqrs.JsonApi/QueryHandlers/GetFirstQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Cqrs.Common.Queries;
+using Cqrs.Core.Abstractions;
+using RestEase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cqrs.JsonApi.QueryHandlers
+{
+    internal class GetFirstQueryHandler<TResource>
+        : IQueryHandlerAsync<GetFirstQuery<TResource>, TResource>
+        where TResource : IResource
+    {
+        public GetFirstQueryHandler(string baseUrl)
+        {
+            RestClient = new RestClient(baseUrl).For<JsonApiBased<TResource>>();
+        }
+
+        public JsonApiBased<TResource> RestClient { get; private set; }
+
+        public Task<TResource> RequestAsync(GetFirstQuery<TResource> query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
