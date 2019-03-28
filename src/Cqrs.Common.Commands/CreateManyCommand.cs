1fda09fca3a525f1b0ccdfd6c46cfbccf69140ad
ASbeletsky <antonbeletsky@gmail.com>
2019-03-29 01:57:03 +0200

    added CreateManyCommand

---
+++ src/Cqrs.Common.Commands/CreateManyCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace Cqrs.Common.Commands
+{
+    #region Using
+    using Cqrs.Core.Abstractions;
+    using System.Collections.Generic;
+    #endregion
+
+    public class CreateManyCommand<T> : ICommand
+    {
+        public CreateManyCommand(params T[] valuesParams) : this(values: valuesParams)
+        {
+        }
+
+        public CreateManyCommand(IEnumerable<T> values)
+        {
+            Values = values;
+        }
+
+        public IEnumerable<T> Values { get; }
+    }
+
+    public class CreateManyCommand<T, TResult> : CreateManyCommand<T>, ICommand<TResult>
+    {
+        public CreateManyCommand(params T[] valuesParams) : base(valuesParams)
+        {
+        }
+
+        public CreateManyCommand(IEnumerable<T> values) : base(values)
+        {
+        }
+    }
+}
