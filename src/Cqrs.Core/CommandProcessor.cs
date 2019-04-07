fdddc918b648d23dc417fbe466990b6e31575b67
ASbeletsky <antonbeletsky@gmail,com>
2019-04-07 23:50:20 +0300

    added xml docs for command processor

--- src/Cqrs.Core/CommandProcessor.cs
+++ src/Cqrs.Core/CommandProcessor.cs
@@ -7,15 +7,26 @@
     using System.Threading.Tasks;
     #endregion
 
+    /// <summary>
+    /// Provides a mechanism for executing commands.
+    /// </summary>
     public class CommandProcessor : ICommandProcessor
     {
         private readonly IServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandProcessor"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider" instance with registred command handlers of appropriate commands. /></param>
         public CommandProcessor(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Executes a command.
+        /// </summary>
+        /// <param name="command">The command to execute</param>
         public void Execute(ICommand command)
         {
             var handlerType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
@@ -23,6 +34,12 @@
             handlerType.GetTypeInfo().GetDeclaredMethod("Apply").Invoke(handler, new object[] { command });
         }
 
+        /// <summary>
+        /// Executes a command with particular result.
+        /// </summary>
+        /// <typeparam name="TResult">Command result type</typeparam>
+        /// <param name="command">The command to execute</param>
+        /// <returns>Result of the executed command</returns>
         public TResult Execute<TResult>(ICommand<TResult> command)
         {
             var handlerType = typeof(ICommandHandler<,>).MakeGenericType(command.GetType(), typeof(TResult));
@@ -30,6 +47,10 @@
             return (TResult)handlerType.GetTypeInfo().GetDeclaredMethod("Apply").Invoke(handler, new object[] { command });
         }
 
+        /// <summary>
+        /// Executes a command asynchronously.
+        /// </summary>
+        /// <param name="command">The command to execute</param>
         public async Task ExecuteAsync(ICommand command)
         {
             var handlerType = typeof(ICommandHandlerAsync<>).MakeGenericType(command.GetType());
@@ -37,6 +58,12 @@
             await(Task) handlerType.GetTypeInfo().GetDeclaredMethod("ApplyAsync").Invoke(handler, new object[] { command });
         }
 
+        /// <summary>
+        /// Executes a command with particular result asynchronously.
+        /// </summary>
+        /// <typeparam name="TResult">Command result type</typeparam>
+        /// <param name="command">The command to execute</param>
+        /// <returns>Result of the executed command</returns>
         public async Task<TResult> ExecuteAsync<TResult>(ICommand<TResult> command)
         {
             var handlerType = typeof(ICommandHandlerAsync<,>).MakeGenericType(command.GetType(), typeof(TResult));
