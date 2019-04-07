2bb41724e3de1ae214baa418017ca4ae0c63ca42
ASbeletsky <antonbeletsky@gmail,com>
2019-04-07 23:50:32 +0300

    added xml docs for query processor

--- src/Cqrs.Core/QueryProcessor.cs
+++ src/Cqrs.Core/QueryProcessor.cs
@@ -7,15 +7,28 @@
     using System.Threading.Tasks;
     #endregion
 
+    /// <summary>
+    /// Provides a mechanism for executing queries.
+    /// </summary>
     public class QueryProcessor : IQueryProcessor
     {
         private readonly IServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QueryProcessor"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider" instance with registred query handlers of appropriate queries. /></param>
         public QueryProcessor(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Executes a query.
+        /// </summary>
+        /// <typeparam name="TResult">Query result type</typeparam>
+        /// <param name="query">The query to execute</param>
+        /// <returns>Result of executed query</returns>
         public TResult Execute<TResult>(IQuery<TResult> query)
         {
             var handlerType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(TResult));
@@ -23,6 +36,12 @@
             return (TResult)handlerType.GetTypeInfo().GetDeclaredMethod("Request").Invoke(handler, new object[] { query });
         }
 
+        /// <summary>
+        /// Executes a query asynchronously.
+        /// </summary>
+        /// <typeparam name="TResult">Query result type</typeparam>
+        /// <param name="query">The query to execute</param>
+        /// <returns>Result of executed query</returns>
         public async Task<TResult> ExecuteAsync<TResult>(IQuery<TResult> query)
         {
             var handlerType = typeof(IQueryHandlerAsync<,>).MakeGenericType(query.GetType(), typeof(TResult));
