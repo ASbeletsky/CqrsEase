f9ebdeafc6c2c895fdab99bc246f9252fcc7291e
ASbeletsky <antonbeletsky@gmail,com>
2019-04-07 23:51:07 +0300

    inherited command with result from command

--- src/Cqrs.Core/Abstractions/ICommand.cs
+++ src/Cqrs.Core/Abstractions/ICommand.cs
@@ -11,7 +11,7 @@
     /// Represents a write operation that changes state of the system with partilar result.
     /// </summary>
     /// <typeparam name="TResult">Command result type</typeparam>
-    public interface ICommand<out TResult>
+    public interface ICommand<out TResult> : ICommand
     {
     }
 }
