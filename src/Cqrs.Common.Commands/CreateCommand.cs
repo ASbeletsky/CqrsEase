844d979f4b2a84f3d36e01b3f4994b5b461c8307
ASbeletsky <antonbeletsky@gmail,com>
2019-04-07 23:51:31 +0300

    added xml docs for create command

--- src/Cqrs.Common.Commands/CreateCommand.cs
+++ src/Cqrs.Common.Commands/CreateCommand.cs
@@ -2,16 +2,27 @@
 {
     using Cqrs.Core.Abstractions;
 
+    /// <summary>
+    /// Represents a command for creating a new data in the system.
+    /// </summary>
+    /// <typeparam name="T">The type of data to create in the system.</typeparam>
     public class CreateCommand<T>
         : ICommand
         , ICommand<ICreateResult<T>>
         where T : class
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateCommand{T}"/> class.
+        /// </summary>
+        /// <param name="value">A data to create in the system.</param>
         public CreateCommand(T value)
         {
             Value = value;
         }
 
+        /// <summary>
+        /// The data to create in the system.
+        /// </summary>
         public T Value { get; }
     }  
 }
--- src/Cqrs.Common.Commands/CreateResult.cs
+++ src/Cqrs.Common.Commands/CreateResult.cs
@@ -1,17 +1,35 @@
 ﻿namespace Cqrs.Common.Commands
 {
+    /// <summary>
+    /// Represents a result of operation for creating a new data in the system.
+    /// </summary>
+    /// <typeparam name="T">The type of data to create in the system.</typeparam>
     public interface ICreateResult<T>
     {
+        /// <summary>
+        /// The data that was created in the system.
+        /// </summary>
         T CreatedValue { get; }
     }
 
+    /// <summary>
+    /// Represents a result of operation for creating a new data in the system.
+    /// </summary>
+    /// <typeparam name="T">The type of data to create in the system.</typeparam>
     public class CreateResult<T> : ICreateResult<T> where T : class
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateResult{T}{T}"/> class.
+        /// </summary>
+        /// <param name="createdValue">The data that was created in the system.</param>
         public CreateResult(T createdValue)
         {
             CreatedValue = createdValue;
         }
 
+        /// <summary>
+        /// The data that was created in the system.
+        /// </summary>
         public T CreatedValue { get; }
     }
 }
