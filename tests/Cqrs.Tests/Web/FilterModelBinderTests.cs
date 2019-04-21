b45c78813adef45cb632bae2cb1386c64cdeac12
ASbeletsky <antonbeletsky@gmail,com>
2019-04-22 01:09:15 +0300

    refactored filter parse test to theory aproach

--- tests/Cqrs.Tests/Web/FilterModelBinderTests.cs
+++ tests/Cqrs.Tests/Web/FilterModelBinderTests.cs
@@ -1,18 +1,17 @@
-﻿using Cqrs.Tests.Model;
-using Cqrs.Web.JsonApi.ModelBinders;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Moq;
-using NSpecifications;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace Cqrs.Tests.Web
+﻿namespace Cqrs.Tests.Web
 {
+    #region Using
+    using Cqrs.Tests.Model;
+    using Cqrs.Web.JsonApi.ModelBinders;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Moq;
+    using NSpecifications;
+    using System.Threading.Tasks;
+    using Xunit;
+    #endregion
+
     public class FilterModelBinderTests
     {
         [Fact]
@@ -52,10 +51,13 @@ namespace Cqrs.Tests.Web
             Assert.Equal(ModelBindingResult.Failed(), bindingContextMock.Object.Result);
         }
 
-        [Fact]
-        public async Task CanParseQueryFilter_WhenSimpleString()
+        [Theory()]
+        [InlineData("?filter[title]=test", "x => (x.Title == \"test\")")]
+        [InlineData("?filter[title]=two+words", "x => (x.Title == \"two words\")")]
+        [InlineData("?filter[title]=test&filter[id]=1", "x => ((x.Title == \"test\") AndAlso (x.Id == \"1\"))")]
+        public async Task CanParseQueryFilter_WithStringValue(string query, string expectedSpecExpression)
         {
-            var filterQueryString = new QueryString("?filter[title]=test");
+            var filterQueryString = new QueryString(query);
             var httpRequestMock = new Mock<HttpRequest>();
             httpRequestMock.Setup(x => x.QueryString).Returns(filterQueryString);
 
@@ -78,36 +80,7 @@ namespace Cqrs.Tests.Web
             Assert.True(bindingContextMock.Object.Model is ASpec<BlogDto>);
 
             var spec = bindingContextMock.Object.Model as ASpec<BlogDto>;
-            Assert.Equal("x => (x.Title == \"test\")", spec.Expression.ToString());
-        }
-
-        [Fact]
-        public async Task CanParseQueryFilter_WhenStringWithSpace()
-        {
-            var filterQueryString = new QueryString("?filter[title]=two+words");
-            var httpRequestMock = new Mock<HttpRequest>();
-            httpRequestMock.Setup(x => x.QueryString).Returns(filterQueryString);
-
-            var httpContextMock = new Mock<HttpContext>();
-            httpContextMock.Setup(x => x.Request).Returns(httpRequestMock.Object);
-
-            var actionContext = new ActionContext();
-            actionContext.HttpContext = httpContextMock.Object;
-
-            var bindingContextMock = new Mock<ModelBindingContext>();
-            bindingContextMock.Setup(x => x.ModelType).Returns(typeof(ISpecification<BlogDto>));
-            bindingContextMock.Setup(x => x.HttpContext).Returns(httpContextMock.Object);
-            bindingContextMock.Setup(x => x.ActionContext).Returns(actionContext);
-            bindingContextMock.SetupProperty(x => x.Model);
-
-            var binder = new FilterModelBinder<BlogDto>();
-            await binder.BindModelAsync(bindingContextMock.Object);
-
-            Assert.NotNull(bindingContextMock.Object.Model);
-            Assert.True(bindingContextMock.Object.Model is ASpec<BlogDto>);
-
-            var spec = bindingContextMock.Object.Model as ASpec<BlogDto>;
-            Assert.Equal("x => (x.Title == \"two words\")", spec.Expression.ToString());
+            Assert.Equal(expectedSpecExpression, spec.Expression.ToString());
         }
     }
 }
