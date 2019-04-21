804445db2eac3a4bca74f403caabf6ee61631b5b
ASbeletsky <antonbeletsky@gmail,com>
2019-04-21 21:25:16 +0300

    added documentation of GetMane ProjectMany queries

--- src/Cqrs.Common.Queries/GetManyQuery.cs
+++ src/Cqrs.Common.Queries/GetManyQuery.cs
@@ -9,23 +9,33 @@
     using System.Collections.Generic;
     #endregion
 
+    /// <summary>
+    /// Represents a query for reading collection of matched data obejct from the system.
+    /// </summary>
+    /// <typeparam name="T">The type of data to read.</typeparam>
     public class GetManyQuery<T> : IQuery<IEnumerable<T>>, IQuery<ILimitedEnumerable<T>>
     {
 
         #region Get all use cases
 
-        public GetManyQuery()
-            : this(sortingParams: null)
-
-        {
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns collection of <typeparamref name="T"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="sortingParams">The ordering used to sort <typeparamref name="T"/> objects.</param>
         public GetManyQuery(params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: new FetchAllStatery<T>(), sortingParams: sortingParams)
         {
         }
 
-        public GetManyQuery(FetchStrategy<T> fetchStrategy, params OrderCreteria<T>[] sortingParams)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns collection of <typeparamref name="T"/> objects according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="T"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="T"/> to populate.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
+        public GetManyQuery(IFetchStrategy<T> fetchStrategy, params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: fetchStrategy, specification: null, pagination: null, sorting: sortingParams)
 
         {
@@ -35,22 +45,48 @@
 
         #region Filter use cases
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns collection of <typeparamref name="T"/> objects that satisfied the <paramref name="specification"/> creteria.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(ISpecification<T> specification)
             : this(specification: specification, sortingParams: null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns collection of <typeparamref name="T"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="T"/> objects.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(ISpecification<T> specification, params OrderCreteria<T>[] sortingParams)
             : this(specification: specification, pagination: null, sortingParams: sortingParams)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns paged collection of <typeparamref name="T"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="T"/> objects.</param>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="T"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(ISpecification<T> specification, IPage pagination, params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: new FetchAllStatery<T>(), specification: specification, pagination: pagination, sorting: sortingParams)
 
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns collection of <typeparamref name="T"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="T"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="T"/> to populate.</param>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="T"/> objects.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(IFetchStrategy<T> fetchStrategy, ISpecification<T> specification, params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: fetchStrategy, specification: specification, pagination: null, sorting: sortingParams)
 
@@ -61,16 +97,35 @@
 
         #region Paging use cases
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns paged collection of <typeparamref name="T"/> objects.
+        /// </summary>
+        /// <param name="pagination"></param>
         public GetManyQuery(IPage pagination)
             : this(pagination: pagination, sortingParams: null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns paged collection of <typeparamref name="T"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="T"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(IPage pagination, params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: new FetchAllStatery<T>(), pagination: pagination, sortingParams: sortingParams)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns paged collection of <typeparamref name="T"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="T"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="T"/> to populate.</param>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="T"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(IFetchStrategy<T> fetchStrategy, IPage pagination, params OrderCreteria<T>[] sortingParams)
             : this(fetchStrategy: fetchStrategy, specification: null, pagination: pagination, sorting: sortingParams)
         {
@@ -78,6 +133,15 @@
 
         #endregion
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetManyQuery{T}"/> class.
+        /// Returns paged collection of <typeparamref name="T"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="T"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="T"/> to populate.</param>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="T"/> objects.</param>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="T"/> objects to a subset.</param>
+        /// <param name="sorting">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
         public GetManyQuery(IFetchStrategy<T> fetchStrategy, ISpecification<T> specification, IPage pagination, IEnumerable<OrderCreteria<T>> sorting)
         {
             Specification = specification;
@@ -86,9 +150,24 @@
             Pagination = pagination;
         }
 
+        /// <summary>
+        /// The members of <typeparamref name="T"/> to populate.
+        /// </summary>
         public IFetchStrategy<T> FetchStrategy { get; }
+
+        /// <summary>
+        /// The creteria used to filter collection of <typeparamref name="T"/> objects.
+        /// </summary>
         public ISpecification<T> Specification { get; }
+
+        /// <summary>
+        /// The creterias used to sort collection of <typeparamref name="T"/> objects.
+        /// </summary>
         public IEnumerable<OrderCreteria<T>> Sorting { get; }
+
+        /// <summary>
+        /// The creteria used to limit rertived collection of <typeparamref name="T"/> objects to a subset.
+        /// </summary>
         public IPage Pagination { get; }
     }
 }
--- src/Cqrs.Common.Queries/ProjectManyQuery.cs
+++ src/Cqrs.Common.Queries/ProjectManyQuery.cs
@@ -1,21 +1,38 @@
 ﻿namespace Cqrs.Common.Queries
 {
-    using System.Collections.Generic;
     #region Using
+    using System.Collections.Generic;
     using Cqrs.Common.Queries.Pagination;
     using Cqrs.Common.Queries.Sorting;
     using NSpecifications;
     #endregion
 
+    /// <summary>
+    /// Represents a query for reading collection of matched <typeparamref name="TSource"/> obejcts and projecting it to collection of <typeparamref name="TDest"/> objects.
+    /// </summary>
+    /// <typeparam name="TSource">The type of data to read.</typeparam>
+    /// <typeparam name="TDest">The type of data to project.</typeparam>
     public class ProjectManyQuery<TSource, TDest> : GetManyQuery<TDest>
     {
         #region Project all use cases
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns collection of <typeparamref name="TDest"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="sortingParams">The ordering used to sort <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(params OrderCreteria<TDest>[] sortingParams) : base(sortingParams)
         {
         }
 
-        public ProjectManyQuery(FetchStrategy<TDest> fetchStrategy, params OrderCreteria<TDest>[] sortingParams) : base(fetchStrategy, sortingParams)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns collection of <typeparamref name="TDest"/> objects according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="TDest"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="T"/> to populate.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="T"/> objects.</param>
+        public ProjectManyQuery(IFetchStrategy<TDest> fetchStrategy, params OrderCreteria<TDest>[] sortingParams) : base(fetchStrategy, sortingParams)
         {
         }
 
@@ -23,18 +40,44 @@
 
         #region Filter uses cases
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns collection of <typeparamref name="TDest"/> objects that satisfied the <paramref name="specification"/> creteria.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(ISpecification<TDest> specification) : base(specification)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns collection of <typeparamref name="TDest"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="TDest"/> objects.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(ISpecification<TDest> specification, params OrderCreteria<TDest>[] sortingParams) : base(specification, sortingParams)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns paged collection of <typeparamref name="TDest"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="TDest"/> objects.</param>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="TDest"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(ISpecification<TDest> specification, IPage pagination, params OrderCreteria<TDest>[] sortingParams) : base(specification, pagination, sortingParams)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns collection of <typeparamref name="TDest"/> objects that satisfied the <paramref name="specification"/> creteria according to the order specified in the <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="TDest"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="TDest"/> to populate.</param>
+        /// <param name="specification">The creteria used to filter collection of <typeparamref name="TDest"/> objects.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(IFetchStrategy<TDest> fetchStrategy, ISpecification<TDest> specification, params OrderCreteria<TDest>[] sortingParams) : base(fetchStrategy, specification, sortingParams)
         {
         }
@@ -43,6 +86,11 @@
 
         #region Paging use cases
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns paged collection of <typeparamref name="TDest"/> objects.
+        /// </summary>
+        /// <param name="pagination"></param>
         public ProjectManyQuery(IPage pagination) : base(pagination)
         {
         }
@@ -51,12 +99,26 @@
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns paged collection of <typeparamref name="TDest"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// </summary>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="TDest"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(IFetchStrategy<TDest> fetchStrategy, IPage pagination, params OrderCreteria<TDest>[] sortingParams) : base(fetchStrategy, pagination, sortingParams)
         {
         }
 
         #endregion
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectManyQuery{TSource, TDest}"/> class.
+        /// Returns paged collection of <typeparamref name="TDest"/> objects according to the specified <paramref name="sortingParams"/> parameter.
+        /// Includes only those <typeparamref name="TDest"/> members that was specified in <paramref name="fetchStrategy"/> parameter.
+        /// </summary>
+        /// <param name="fetchStrategy">The members of <typeparamref name="TDest"/> to populate.</param>
+        /// <param name="pagination">The creteria used to limit rertived collection of <typeparamref name="TDest"/> objects to a subset.</param>
+        /// <param name="sortingParams">The creterias used to sort collection of <typeparamref name="TDest"/> objects.</param>
         public ProjectManyQuery(IFetchStrategy<TDest> fetchStrategy, ISpecification<TDest> specification, IPage pagination, IEnumerable<OrderCreteria<TDest>> sorting) : base(fetchStrategy, specification, pagination, sorting)
         {
         }
