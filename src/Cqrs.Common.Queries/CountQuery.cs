72cc52d6d17ff23b2b5b4f0b8bd203b597b857cd
ASbeletsky <antonbeletsky@gmail,com>
2019-04-21 21:45:11 +0300

    added docs for CountQuery

--- src/Cqrs.Common.Queries/CountQuery.cs
+++ src/Cqrs.Common.Queries/CountQuery.cs
@@ -1,15 +1,28 @@
-﻿using Cqrs.Core.Abstractions;
-using NSpecifications;
-
-namespace Cqrs.Common.Queries
+﻿namespace Cqrs.Common.Queries
 {
+    #region Using
+    using Cqrs.Core.Abstractions;
+    using NSpecifications;
+    #endregion
+
+    /// <summary>
+    /// Represents a query for count objects that matches specified creteria.
+    /// </summary>
+    /// <typeparam name="T">The type of data objects to count.</typeparam>
     public class CountQuery<T> : IQuery<long>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountQuery{T}"/> class.
+        /// </summary>
+        /// <param name="specification"></param>
         public CountQuery(ISpecification<T> specification)
         {
             Specification = specification;
         }
 
+        /// <summary>
+        /// The creteria used to search <typeparamref name="T"/> objects.
+        /// </summary>
         public ISpecification<T> Specification { get; }
     }
 }
