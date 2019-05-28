0e26a7df9c3841edb746809c1e8e7d65b92c38b8
beletsky.anton <beletsky.anton@apriorit.com>
2019-05-28 20:03:17 +0300

    added utils for building http query string with json api params

--- src/Cqrs.JsonApi/Web/Request/JsonApiExtensions.cs
+++ src/Cqrs.JsonApi/Web/Request/JsonApiExtensions.cs
@@ -1,8 +1,7 @@
-﻿namespace Cqrs.JsonApi
+﻿namespace Cqrs.JsonApi.Web.Request
 {
     #region Using
     using Cqrs.Common.Queries;
-    using Cqrs.Common.Queries.Pagination;
     using Cqrs.Common.Queries.Sorting;
     using NSpecifications;
     using System;
@@ -19,44 +18,34 @@
             var resouceBaseType = typeof(IResource).GetTypeInfo();
             var rootResourceProperties = typeof(TResource).GetTypeInfo().DeclaredProperties;
             var relatedResourcesProperies = rootResourceProperties.Where(p => resouceBaseType.IsAssignableFrom(p.PropertyType.GetTypeInfo()));
-            
+
             if (relatedResourcesProperies != null && relatedResourcesProperies.Any())
             {
 
                 var relatedResourcesToInlude = fetchStrategy.FetchedPaths.Where(path => relatedResourcesProperies.Any(prop => path.Contains(prop.Name)));
                 var includedResourcesString = string.Join(",", relatedResourcesToInlude);
-                return $"include={includedResourcesString}";
+                return includedResourcesString;
             }
 
             return string.Empty;
         }
 
-        internal static string ToJsonApiSparseFields<TResource>(this IFetchStrategy<TResource> fetchStrategy) where TResource : IResource, new()
+        internal static IDictionary<string, string> ToJsonApiSparseFields<TResource>(this IFetchStrategy<TResource> fetchStrategy) where TResource : IResource, new()
         {
-            var resouceBaseType = typeof(IResource).GetTypeInfo();
+            var result = new Dictionary<string, string>();
             var resourceType = Activator.CreateInstance<TResource>();
             var relatedResourceSparseFields = fetchStrategy.FetchedPaths.Where(path => path.Contains("."));
             var rootResourceSparseFields = fetchStrategy.FetchedPaths.Except(relatedResourceSparseFields);
 
             string rootResourceSparseFieldsString = string.Join(",", rootResourceSparseFields);
-            string result = $"fields[{resourceType.Type}]={rootResourceSparseFieldsString}";
+            result.Add(resourceType.Type, rootResourceSparseFieldsString);
 
-            if(relatedResourceSparseFields.Any())
+            if (relatedResourceSparseFields.Any())
             {
                 //TODO: finish fileds to related reources
-
-                string relatedResourceSparseFieldsString = string.Join(",", relatedResourceSparseFields);
-                result += $"fields[{resourceType.Type}]={rootResourceSparseFieldsString}";
             }
 
             return result;
-        }
-        
-
-        internal static string ToJsonApiSorting<TResource>(this IEnumerable<OrderCreteria<TResource>> orderCreterias) where TResource : IResource
-        {
-            string sortFields = string.Join(",", orderCreterias.Select(c => c.ToJsonApiParameter()));
-            return $"sort={sortFields}";
         }
 
         internal static string ToJsonApiParameter<TResource>(this OrderCreteria<TResource> orderCreteria) where TResource : IResource
@@ -64,36 +53,31 @@
             return $"{orderCreteria.Direction}{orderCreteria.SortKey}";
         }
 
-        internal static string ToJsonApiPaging(this IPage page)
-        {
-            return $"page[size]={page.Size}&page[number]={page.Number}";
-        }
-
         #region Filtering
 
-        internal static string ToJsonApiFilter<TResource>(this ISpecification<TResource> specification) where TResource : IResource
+        internal static IDictionary<string, string> ToJsonApiFilter<TResource>(this ISpecification<TResource> specification) where TResource : IResource
         {
             var lambda = specification.Expression;
-            return BuildFilterParams(lambda.Body);
+            var result = new Dictionary<string, string>();
+            BuildFilterParams(lambda.Body, ref result);
+            return result;
         }
 
-        private static string BuildFilterParams(Expression expression)
+        private static void BuildFilterParams(Expression expression, ref Dictionary<string, string> result)
         {
-            string filterParams = string.Empty;
             if (expression.NodeType == ExpressionType.Equal)
             {
                 BinaryExpression eq = (BinaryExpression)expression;
                 MemberExpression proteprtySelector = (MemberExpression)eq.Left;
                 string propertyName = proteprtySelector.Member.Name;
                 object value = Evaluate(eq.Right);
-                filterParams = $"filter[{propertyName}]={value}";
+                result.Add(propertyName, value.ToString());
             }
             else if (expression is BinaryExpression binaryExpression)
             {
-                filterParams = string.Join("&", BuildFilterParams(binaryExpression.Left), BuildFilterParams(binaryExpression.Right));
+                BuildFilterParams(binaryExpression.Left, ref result);
+                BuildFilterParams(binaryExpression.Right, ref result);
             }
-
-            return filterParams;
         }
 
         private static object Evaluate(Expression e)
---
+++ src/Cqrs.JsonApi/Web/Request/QueryBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace Cqrs.JsonApi.Web.Request
+{
+    #region Using
+    using Cqrs.Common.Queries;
+    using Cqrs.Common.Queries.Pagination;
+    using Cqrs.Common.Queries.Sorting;
+    using Microsoft.AspNetCore.Http.Extensions;
+    using NSpecifications;
+    using System.Collections.Generic;
+    using System.Linq;
+    #endregion
+
+    internal static class QueryBuilderExtensions
+    {
+        internal static QueryBuilder AddSparseFieldsets<TResource>(this QueryBuilder queryBuilder, IFetchStrategy<TResource> fetchStrategy) where TResource : IResource, new()
+        {
+            var resourcesSparseFields = fetchStrategy.ToJsonApiSparseFields();
+            foreach (var resourceSparseFields in resourcesSparseFields)
+            {
+                var paramName = string.Format(JsonApiQueryParams.SparseFieldsetsTemplate, resourceSparseFields.Key);
+                var paramValue = resourceSparseFields.Value;
+                queryBuilder.Add(paramName, paramValue);
+            }
+
+            return queryBuilder;
+        }
+
+        internal static QueryBuilder AddIncludedResources<TResource>(this QueryBuilder queryBuilder, IFetchStrategy<TResource> fetchStrategy) where TResource : IResource
+        {
+            queryBuilder.Add(JsonApiQueryParams.Include, fetchStrategy.ToJsonApiInclude());
+            return queryBuilder;
+        }
+
+        internal static QueryBuilder AddFilter<TResource>(this QueryBuilder queryBuilder, ISpecification<TResource> specification) where TResource : IResource
+        {
+            var resourceFieldsFilters = specification.ToJsonApiFilter();
+            foreach (var resourceFieldsFilter in resourceFieldsFilters)
+            {
+                var paramName = string.Format(JsonApiQueryParams.FieldFilterTemplate, resourceFieldsFilter.Key);
+                var paramValue = resourceFieldsFilter.Value;
+                queryBuilder.Add(paramName, paramValue);
+            }
+
+            return queryBuilder;
+        }
+
+        internal static QueryBuilder AddPaging(this QueryBuilder queryBuilder, IPage page)
+        {
+            queryBuilder.Add(JsonApiQueryParams.PageSize, page.Size.ToString());
+            queryBuilder.Add(JsonApiQueryParams.PageNumber, page.Number.ToString());
+            return queryBuilder;
+        }
+
+        internal static QueryBuilder AddSorting<TResource>(this QueryBuilder queryBuilder, IEnumerable<OrderCreteria<TResource>> orderCreterias) where TResource : IResource
+        {
+            string sortFields = string.Join(",", orderCreterias.Select(c => c.ToJsonApiParameter()));
+            queryBuilder.Add(JsonApiQueryParams.Sorting, sortFields);
+            return queryBuilder;
+        }
+    }
+}
