fb9a8ed205436aab3b92dd8c6f6a0b13c2110ef8
beletsky.anton <beletsky.anton@apriorit.com>
2019-05-29 19:33:00 +0300

    added jsonapi GetManyQuery implementation and tests

---
+++ src/Cqrs.JsonApi/QueryHandlers/GetManyQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace Cqrs.JsonApi.QueryHandlers
+{
+    #region 
+    using Cqrs.Common.Queries;
+    using Cqrs.Common.Queries.Pagination;
+    using Cqrs.Core.Abstractions;
+    using Cqrs.JsonApi.Web;
+    using Cqrs.JsonApi.Web.Request;
+    using Microsoft.AspNetCore.Http.Extensions;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    #endregion
+
+    public class GetManyQueryHandler<TResource>
+        : IQueryHandlerAsync<GetManyQuery<TResource>, IEnumerable<TResource>>
+        where TResource : IResource, new()
+    {
+        public GetManyQueryHandler(string baseUrl)
+        {
+            ResourceEndpoint = new JsonApiClient(baseUrl).For<IJsonApiEndpoint<TResource>>();
+        }
+
+        public IJsonApiEndpoint<TResource> ResourceEndpoint { get; private set; }
+
+        protected string BuildQueryString(GetManyQuery<TResource> query)
+        {
+            var queryString = new QueryBuilder()
+                .MaybeAddIncludedResources(query.FetchStrategy)
+                .MaybeAddFilter(query.Specification)
+                .MaybeAddPaging(query.Pagination)
+                .MaybeAddSorting(query.Sorting)
+                .MaybeAddSparseFieldsets(query.FetchStrategy)
+                .ToQueryString();
+
+            return queryString.Value;
+        }
+
+        public async Task<IEnumerable<TResource>> RequestAsync(GetManyQuery<TResource> query)
+        {
+            var queryString = BuildQueryString(query);
+            return await ResourceEndpoint.Get(queryString);
+        }
+    }
+}
