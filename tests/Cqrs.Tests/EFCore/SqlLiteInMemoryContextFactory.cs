33a2b7df5b665bcb1c3d5b3157f42442d683ee13
ASbeletsky <antonbeletsky@gmail.com>
2019-05-31 03:05:33 +0300

    added sqllite for testing

---
+++ tests/Cqrs.Tests/EFCore/SqlLiteInMemoryContextFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+
+namespace Cqrs.Tests.EFCore
+{
+    class SqlLiteInMemoryContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+
+        private DbContextOptions<BloggingContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<BloggingContext>()
+                .UseSqlite(_connection).Options;
+        }
+
+        public BloggingContext CreateContext()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                var options = CreateOptions();
+                using (var context = new BloggingContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+
+            return new BloggingContext(CreateOptions());
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
